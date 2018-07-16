f0947bd73c8a58dd008ceda8f1322ba30a76c34e
Donna Cerrio <denique86@yahoo.com>
2018-07-16 23:05:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  // Write your code in this file!
 const currentUser = 'Grace Hooper';
-const welcomeMessage =`Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${curentUser.slice(0,1)}!`;
