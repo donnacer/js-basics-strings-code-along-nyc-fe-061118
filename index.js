62bb35ea8d67c4580da7885541ff085286239302
Donna Cerrio <denique86@yahoo.com>
2018-07-16 23:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Grace Hooper';
 const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${curentUser.slice(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
